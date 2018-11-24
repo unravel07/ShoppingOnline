1c6a4cb9843154865380a2a3f231ff158371d48c
lonelycay99 <nerddy.hahaha@hotmail.com>
2018-11-24 16:15:18 +0700

    Implement Product detail

--- ShoppingOnline/src/pages/product-detail/product-detail.ts
+++ ShoppingOnline/src/pages/product-detail/product-detail.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../../app/Model';
+import { CartPage } from '../cart/cart';
 
 /**
  * Generated class for the ProductDetailPage page.
@@ -14,12 +17,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'product-detail.html',
 })
 export class ProductDetailPage {
+  Pid : any
+  products: Product = new Product;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: HttpClient) {
+    this.http.get<Product>("http://localhost:5000/api/Shooping/GetProduct/"+this.Pid)
+    .subscribe(data => {
+      this.products = data
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProductDetailPage');
+  }
+
+  addCart(){
+    this.navCtrl.push(CartPage,{id:this.Pid});
   }
 
 }
