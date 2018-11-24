809b99ea8bdd157973efa22501e6137c61dc466e
thaninpong <thaninpong@perfenterprise.com>
2018-11-24 15:31:25 +0700

    Implement Product

---
+++ ShoppingOnline/src/pages/modaleditproduct/modaleditproduct.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ModaleditproductPage } from './modaleditproduct';
+
+@NgModule({
+  declarations: [
+    ModaleditproductPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ModaleditproductPage),
+  ],
+})
+export class ModaleditproductPageModule {}
