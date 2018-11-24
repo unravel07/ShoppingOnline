1f157b4828da308ff741bb73256cce5c14db6427
tatae <tachapong999.gmail.com>
2018-11-24 16:07:42 +0700

    cart binding

--- ShoppingOnline/src/pages/cart/cart.ts
+++ ShoppingOnline/src/pages/cart/cart.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../../app/Model';
 
 /**
  * Generated class for the CartPage page.
@@ -15,38 +17,72 @@ import { AlertController } from 'ionic-angular';
   templateUrl: 'cart.html',
 })
 export class CartPage {
+  result: number;
+  amount: number;
+  price: number;
+  address: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController) {
+  getProducts: Product[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public http: HttpClient) {
+
+    this.http.get<Product[]>("http://localhost:5000/api/Shopping/GetProductInCart").subscribe(
+      it => {
+        this.getProducts = it
+        this.funcCal(this.getProducts);
+      },
+      error => {
+        // ERROR: Do something
+      });
+  }
+
+  funcCal(data) {
+    console.log(data)
+    for (var i = 0; i < data.length; i++) {
+    
+      this.result = this.result+(data[i].amount * data[i].prices);
+    }
+    console.log(this.result)
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CartPage');
   }
 
-  Purchase()
-  {
-         const confirm = this.alertCtrl.create({
-        title: 'ยืนยันการสั่งซื้อ',
-       
-        buttons: [
-          {
-            text: 'ยันยืน',
-            handler: () => {
-              console.log('Disagree clicked');
-            }
-          },
-          {
-            text: 'ยกเลิก',
-            handler: () => {
-              console.log('Agree clicked');
-            }
-          }
-        ]
-      });
-      confirm.present();
-    }
-  
+  Purchase() {
+    const confirm = this.alertCtrl.create({
+      title: 'ยืนยันการสั่งซื้อ',
 
-  
+      buttons: [
+        {
+          text: 'ยันยืน',
+          handler: () => {
+            // this.http.post<YOUR_MODEL_NAME>("ENTER_YOUR_API_HERE",
+            //   {
+            //     // PROPERTY_NAME_1: VALUE_1,
+            //     // PROPERTY_NAME_2: VALUE_2
+            //   }).subscribe(
+            //     it => {
+            //       // SUCCESS: Do something
+            //     },
+            //     error => {
+            //       // ERROR: Do something
+            //     });
+            // console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'ยกเลิก',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+
+
 
 }
