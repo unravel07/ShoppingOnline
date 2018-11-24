b799b59f9da34face791fe74741fc543fb26ea79
apiched1 <apiched_oh@hotmail.com>
2018-11-24 11:23:10 +0700

    Imprement UI Page ProductDetail

--- ShoppingOnline/src/app/app.component.ts
+++ ShoppingOnline/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { ProductDetailPage } from '../pages/product-detail/product-detail';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = ProductDetailPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
